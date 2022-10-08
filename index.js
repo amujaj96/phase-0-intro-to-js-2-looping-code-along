8261a149c06129d2a0dccdd81ba01d14b347415e
Agim <dawnhillsnyc@gmail.com>
2022-10-08 19:42:51 -0400

    commit tree ee4f513860d0a65328580b3213eb6e4a59031270

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+const names = ['Guadalupe', 'Ollie', "Aki"]
+
+function wrapGifts(names) {
+    for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you, ${names}, for the wonderful birthday gift`)
+    debugger;
+}
+
+return names;
+}
+
+wrapGifts(names, "birthday")
